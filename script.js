a5409c9bd5e9f7dedced09a12be7c16b47853dc5
rb-business-center <cthierrymichel@gmail.com>
2026-03-30 14:45:42 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 // JavaScript source code
-console.log("RB BUSINESS CENTER en ligne");
+function commander(nomProduit) {
+
+    let numeros = [
+        "22667913463",
+        "22671014634"
+    ];
+
+    // Message personnalisé
+    let message = "Nous sommes le monument des accessoires\n\n";
+    message += "Produit : " + nomProduit + "\n";
+    message += "Merci de nous contacter pour commander.";
+
+    // On choisit le premier numéro (tu peux changer)
+    let numero = numeros[0];
+
+    let url = "https://wa.me/" + numero + "?text=" + encodeURIComponent(message);
+
+    window.open(url, "_blank");
+}
